669bfba1f186bac40914aed5a2a3f8a4de9856f4
Oscar Timm Marcussen <oscar.marcussen@aboveit.no>
2023-10-11 09:52:43 +0200

    start

--- cultimate/App.js
+++ cultimate/App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Navigation from './src/navigation/navigation';
+import LoginScreen from './src/screens/login/loginScreen';
 
 export default function App() {
   return (
-    <Navigation/>
+    <LoginScreen />
+    // <Navigation/>
   );
 }
---
+++ cultimate/src/screens/login/loginScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const LoginScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Welcome to the HomeScreen!</Text>
+    </View>
+  );
+}
+
+export default LoginScreen;
