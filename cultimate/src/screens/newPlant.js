f3e361904bf46ca077c56db89f0840d10e30f811
Annabdoan <71780695+Annabdoan@users.noreply.github.com>
2023-10-11 20:24:23 +0200

    feat: modal for plant suggestion

--- cultimate/src/screens/newPlant.js
+++ cultimate/src/screens/newPlant.js
@@ -1,30 +1,87 @@
-import React from 'react';
-import { View, Text, FlatList, TouchableOpacity, Button, StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import ListaPlanta from '../components/ListaPlanta';
-import { createStackNavigator } from '@react-navigation/stack';
-import infoplanta from './infoplanta';
-import { useNavigation } from '@react-navigation/native';
-import ButtonTabs from '../navigation/ventanas';
-import Infoplanta from './infoplanta';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React, { useState } from "react";
+import { View, Text, Pressable } from "react-native";
+import ListaPlanta from "../components/ListaPlanta";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { SuggestionModal } from "./suggestionModal";
 
 const Tab = createBottomTabNavigator();
 
-const NewPlant = ({navigation}) => {
+const NewPlant = ({ navigation }) => {
   const data = [
-    { id: 2, nombre: 'Fresas', Descripcion: 'Las fresas son una planta frutal deliciosa', DificultadPlantado: 2, Tipo: 'Exterior', EstacionRecomendada: 'Primavera', FuncionalidadPlanta: 'Postre/Reposteria' , IniFechaPlantado: '2023-04-01', FinFechaPlantado: '2023-06-30', PeriodicidadRegado: 2, CondLuminosas: 'Luz directa', CondTemperatura: 'Moderada', TamMaceta: 12, CategoriaID: 1 },
-    { id: 3, nombre: 'Mora', Descripcion: 'Las moras son frutas deliciosas y versátiles', DificultadPlantado: 3, Tipo: 'Exterior', EstacionRecomendada: 'Verano', FuncionalidadPlanta: 'Postre/Reposteria' , IniFechaPlantado: '2023-06-01', FinFechaPlantado: '2023-09-30', PeriodicidadRegado: 2, CondLuminosas: 'Luz directa', CondTemperatura: 'Moderada', TamMaceta: 15, CategoriaID: 1 },
-    { id: 4, nombre: 'Limonero', Descripcion: 'El limonero es un árbol frutal conocido por sus limones', DificultadPlantado: 2, Tipo: 'Exterior', EstacionRecomendada: 'Primavera', FuncionalidadPlanta: 'Producción de limones' , IniFechaPlantado: '2023-03-01', FinFechaPlantado: '2023-10-31', PeriodicidadRegado: 3, CondLuminosas: 'Luz directa', CondTemperatura: 'Cálida', TamMaceta: 20, CategoriaID: 1 },
-  ]
+    {
+      id: 2,
+      nombre: "Fresas",
+      Descripcion: "Las fresas son una planta frutal deliciosa",
+      DificultadPlantado: 2,
+      Tipo: "Exterior",
+      EstacionRecomendada: "Primavera",
+      FuncionalidadPlanta: "Postre/Reposteria",
+      IniFechaPlantado: "2023-04-01",
+      FinFechaPlantado: "2023-06-30",
+      PeriodicidadRegado: 2,
+      CondLuminosas: "Luz directa",
+      CondTemperatura: "Moderada",
+      TamMaceta: 12,
+      CategoriaID: 1,
+    },
+    {
+      id: 3,
+      nombre: "Mora",
+      Descripcion: "Las moras son frutas deliciosas y versátiles",
+      DificultadPlantado: 3,
+      Tipo: "Exterior",
+      EstacionRecomendada: "Verano",
+      FuncionalidadPlanta: "Postre/Reposteria",
+      IniFechaPlantado: "2023-06-01",
+      FinFechaPlantado: "2023-09-30",
+      PeriodicidadRegado: 2,
+      CondLuminosas: "Luz directa",
+      CondTemperatura: "Moderada",
+      TamMaceta: 15,
+      CategoriaID: 1,
+    },
+    {
+      id: 4,
+      nombre: "Limonero",
+      Descripcion: "El limonero es un árbol frutal conocido por sus limones",
+      DificultadPlantado: 2,
+      Tipo: "Exterior",
+      EstacionRecomendada: "Primavera",
+      FuncionalidadPlanta: "Producción de limones",
+      IniFechaPlantado: "2023-03-01",
+      FinFechaPlantado: "2023-10-31",
+      PeriodicidadRegado: 3,
+      CondLuminosas: "Luz directa",
+      CondTemperatura: "Cálida",
+      TamMaceta: 20,
+      CategoriaID: 1,
+    },
+  ];
+
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
 
   return (
-    
-   
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <ListaPlanta data = {data} navigation = {navigation}/>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <ListaPlanta data={data} navigation={navigation} />
+      <Pressable
+         style={{
+          backgroundColor: "lightgreen",
+          padding: 10,
+          borderRadius: 20,
+          elevation: 2,
+        }}
+        onPress={toggleModal}
+      >
+        <Text>Suggest new plant</Text>
+      </Pressable>
+      <SuggestionModal isVisible={isModalVisible} closeModal={toggleModal}/>
+
     </View>
-    );
-}
+  );
+};
 
 export default NewPlant;
