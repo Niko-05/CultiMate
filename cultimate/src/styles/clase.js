f8284476f46df1a52464ce7aa14b9461c7caa7b4
Yavae <98843718+Yavae@users.noreply.github.com>
2023-10-13 21:52:33 +0200

    cositas

---
+++ cultimate/src/styles/clase.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+export const GuiaPlantadoStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  plantInfoContainer: {
+    marginVertical: 10,
+  },
+  infoLabel: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  infoText: {
+    fontSize: 14,
+  },
+});
