2682c091f9ac3937139d06982d1f1d34998a35b0
Yavae <98843718+Yavae@users.noreply.github.com>
2023-10-13 21:52:33 +0200

    cositas

--- cultimate/src/screens/guiaPlantado.js
+++ cultimate/src/screens/guiaPlantado.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import { View, Text, FlatList, TouchableOpacity, Button, StyleSheet } from 'react-native';
+import { GuiaPlantadoStyles } from '../styles/clase.js';
 
-const GuiaPlantado = () => {}
+  
+
+const GuiaPlantado = () => {
+return (
+    <View style={GuiaPlantadoStyles.container}>
+      <Text style={GuiaPlantadoStyles.headerText}>Guía de Plantado</Text>
+      <View style={GuiaPlantadoStyles.plantInfoContainer}>
+        <Text style={GuiaPlantadoStyles.infoLabel}>Planta:</Text>
+        <Text style={GuiaPlantadoStyles.infoText}>Tomate</Text>
+      </View>
+      <View style={GuiaPlantadoStyles.plantInfoContainer}>
+        <Text style={GuiaPlantadoStyles.infoLabel}>Fecha de Plantado:</Text>
+        <Text style={GuiaPlantadoStyles.infoText}>25 de octubre, 2023</Text>
+      </View>
+      {/* Otros elementos aquí */}
+    </View>
+    );
+}
 
 export default GuiaPlantado;
