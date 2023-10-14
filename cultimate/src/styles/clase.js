5c55f30a508314057d17aff68d2da84636be8f11
Yavae <98843718+Yavae@users.noreply.github.com>
2023-10-14 19:41:58 +0200

    Modificación del archivo guiaPlantado.js Modificación del archivo clase.js añadiendo el estilo de algunos componentes Modificación del frontend de la guía de plantado

--- cultimate/src/styles/clase.js
+++ cultimate/src/styles/clase.js
@@ -1,39 +1,83 @@
+import { cloneElement } from 'react';
 import { StyleSheet } from 'react-native';
 
 export const GuiaPlantadoStyles = StyleSheet.create({
+  
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'top',
+    backgroundColor: '#fff',
+    margin: '10%',
+    marginBottom: '15%',
+    marginTop: '15%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    
   },
-  headerText: {
-    fontSize: 20,
-    fontWeight: 'bold',
+
+  container2: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: '10%',
+    borderColor: 'lightgrey',
+    borderRadius: 15,
+    margin: '2%',
+    marginTop: '5%',
+    marginBottom: '5%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    backgroundColor: '#c0e8c0',
   },
+
+  pagina: {
+    flex: 1,
+    backgroundColor: '#fff',
+    borderRadius: 15,
+    padding : '5%',
+    margin: '2%',
+  },
+
   plantInfoContainer: {
     marginVertical: 10,
     flexDirection: 'row',
+    textAlign: 'justify',
+    flexDirection: 'row',
+    flexGrow: 30,
+    flexWrap: 'wrap',
 
   },
-  infoLabel: {
+  titulo: {
     fontSize: 16,
     fontWeight: 'bold',
     marginBottom: 5,
+    marginLeft: 10,
   },
   infoText: {
     fontSize: 16,
-  },
-  containerFila:{
-    flexDirection: 'row',
-    alignItems:'center',
-    justifyContent: 'center',
+    flexWrap: 'wrap',
+    textAlign: 'justify',
+    paddingLeft: 10,
   },
   image:{
-    height: 50,
-    width: 50,
-    margin: '5%'
+    height: 64,
+    width: 64,
+    marginRight: '5%',
   },
   button:{
-    color: '#71d772', 
+    color: '#blue', 
+    backgroundColor: '#blue',
+  },
+  textbutton:{
+    color: 'darkgreen', 
+  },
+  fondobutton: {
+    backgroundColor: 'lightgreen',
+    padding: 10,
+    borderRadius: 15,
+  },
+  separador: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+    marginVertical: 16
+    ,
   },
 });
