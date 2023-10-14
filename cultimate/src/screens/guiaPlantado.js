b8128a7ef2d786aced8f92c0d3936eb9dc3dc19e
Yavae <98843718+Yavae@users.noreply.github.com>
2023-10-14 19:41:58 +0200

    Modificación del archivo guiaPlantado.js Modificación del archivo clase.js añadiendo el estilo de algunos componentes Modificación del frontend de la guía de plantado

--- cultimate/src/screens/guiaPlantado.js
+++ cultimate/src/screens/guiaPlantado.js
@@ -1,59 +1,69 @@
 import React from 'react';
-import { View, Text, FlatList, TouchableOpacity, Button, StyleSheet, Image } from 'react-native';
+import { ScrollView, View, Text, FlatList, TouchableOpacity, Button, StyleSheet, Image } from 'react-native';
 import { GuiaPlantadoStyles } from '../styles/clase.js';
 
   
 
 const GuiaPlantado = () => {
 return (
-    <View style={GuiaPlantadoStyles.container}>
+    <ScrollView style={GuiaPlantadoStyles.pagina}>
       <View style={GuiaPlantadoStyles.plantInfoContainer}>
-        <Text style={GuiaPlantadoStyles.infoLabel}>Planta: </Text>
+        <Text style={GuiaPlantadoStyles.titulo}>Planta: </Text>
         <Text style={GuiaPlantadoStyles.infoText}>Tomate</Text>
       </View>
 
       <View style={GuiaPlantadoStyles.plantInfoContainer}>
-        <Text style={GuiaPlantadoStyles.infoLabel}>Fecha de Plantado: </Text>
+        <Text style={GuiaPlantadoStyles.titulo}>Fecha de Plantado: </Text>
         <Text style={GuiaPlantadoStyles.infoText}>25 de octubre, 2023</Text>
       </View>
+
       
-      <View style={GuiaPlantadoStyles.containerFila}>
+      <View style={GuiaPlantadoStyles.container2}>
         <Image style = {GuiaPlantadoStyles.image} source= {require("../../assets/regar.png")}/>
         <Text style={GuiaPlantadoStyles.infoText}>Regar 2 veces al día</Text>
       </View>
 
-      <View style={GuiaPlantadoStyles.containerFila}>
+
+      <View style={GuiaPlantadoStyles.container2}>
         <Image style = {GuiaPlantadoStyles.image} source= {require("../../assets/termometro.png")}/>
-        <Text style={GuiaPlantadoStyles.infoText}>20ºC - 25ºC</Text>
+        <Text style={GuiaPlantadoStyles.infoText}>20ºC - 25ºC </Text>
       </View>
 
-      <View style={GuiaPlantadoStyles.containerFila}>
+
+      <View style={GuiaPlantadoStyles.container2}>
         <Image style = {GuiaPlantadoStyles.image} source= {require("../../assets/planta.png")}/>
         <Text style={GuiaPlantadoStyles.infoText}>Maceta mediana</Text>
       </View>
-      
+
+        
       <View>
         <View style={GuiaPlantadoStyles.plantInfoContainer}>
-          <Text style={GuiaPlantadoStyles.infoLabel}>Paso: </Text>
+          <Text style={GuiaPlantadoStyles.titulo}>Paso: </Text>
           <Text style={GuiaPlantadoStyles.infoText}> 1</Text>
         </View>
-
+        
         <View style={GuiaPlantadoStyles.plantInfoContainer}>
-          <Text style={GuiaPlantadoStyles.infoLabel}>Detalles: </Text>
-          <Text style={GuiaPlantadoStyles.infoText}> Preparar la maceta y el sustrato</Text>
+          <Text style={GuiaPlantadoStyles.titulo}>Detalles: </Text>
+          <Text style={GuiaPlantadoStyles.infoText}>Preparar la maceta y el sustrato </Text>
         </View>
 
         <View style={GuiaPlantadoStyles.plantInfoContainer}>
-          <Text style={GuiaPlantadoStyles.infoLabel}>Estado de la Planta: </Text>
-          <Text style={GuiaPlantadoStyles.infoText}> Preparar la maceta y el sustrato</Text>
+          <Text style={GuiaPlantadoStyles.titulo}>Estado de la Planta: </Text>
+          <Text style={GuiaPlantadoStyles.infoText}>Preparar la maceta y el sustrato </Text>
         </View>
 
+        <View style={GuiaPlantadoStyles.separador} />
+
         <View style={GuiaPlantadoStyles.plantInfoContainer}>
-          <Button color={'#5ac15d'} title='¿Qué hago ahora?'/>
+          <View style={GuiaPlantadoStyles.fondobutton}>
+            <TouchableOpacity style={GuiaPlantadoStyles.button}>
+              <Text style={GuiaPlantadoStyles.textbutton}>¿Qué hago ahora?</Text>
+              </TouchableOpacity>
+          </View>
         </View>
 
       </View>
-    </View>
+    </ScrollView>
     );
 }
 
