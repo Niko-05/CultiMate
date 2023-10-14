3c20581f2c050fcfc66e4bf3b4a0ff19794f1ee4
Niko-05 <nicolasmz0511@gmail.com>
2023-10-14 14:15:29 +0200

    Nuevas secciones en la ventana

--- cultimate/src/screens/guiaPlantado.js
+++ cultimate/src/screens/guiaPlantado.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, FlatList, TouchableOpacity, Button, StyleSheet } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, Button, StyleSheet, Image } from 'react-native';
 import { GuiaPlantadoStyles } from '../styles/clase.js';
 
   
@@ -7,16 +7,52 @@ import { GuiaPlantadoStyles } from '../styles/clase.js';
 const GuiaPlantado = () => {
 return (
     <View style={GuiaPlantadoStyles.container}>
-      <Text style={GuiaPlantadoStyles.headerText}>Guía de Plantado</Text>
       <View style={GuiaPlantadoStyles.plantInfoContainer}>
-        <Text style={GuiaPlantadoStyles.infoLabel}>Planta:</Text>
+        <Text style={GuiaPlantadoStyles.infoLabel}>Planta: </Text>
         <Text style={GuiaPlantadoStyles.infoText}>Tomate</Text>
       </View>
+
       <View style={GuiaPlantadoStyles.plantInfoContainer}>
-        <Text style={GuiaPlantadoStyles.infoLabel}>Fecha de Plantado:</Text>
+        <Text style={GuiaPlantadoStyles.infoLabel}>Fecha de Plantado: </Text>
         <Text style={GuiaPlantadoStyles.infoText}>25 de octubre, 2023</Text>
       </View>
-      {/* Otros elementos aquí */}
+      
+      <View style={GuiaPlantadoStyles.containerFila}>
+        <Image style = {GuiaPlantadoStyles.image} source= {require("../../assets/regar.png")}/>
+        <Text style={GuiaPlantadoStyles.infoText}>Regar 2 veces al día</Text>
+      </View>
+
+      <View style={GuiaPlantadoStyles.containerFila}>
+        <Image style = {GuiaPlantadoStyles.image} source= {require("../../assets/termometro.png")}/>
+        <Text style={GuiaPlantadoStyles.infoText}>20ºC - 25ºC</Text>
+      </View>
+
+      <View style={GuiaPlantadoStyles.containerFila}>
+        <Image style = {GuiaPlantadoStyles.image} source= {require("../../assets/planta.png")}/>
+        <Text style={GuiaPlantadoStyles.infoText}>Maceta mediana</Text>
+      </View>
+      
+      <View>
+        <View style={GuiaPlantadoStyles.plantInfoContainer}>
+          <Text style={GuiaPlantadoStyles.infoLabel}>Paso: </Text>
+          <Text style={GuiaPlantadoStyles.infoText}> 1</Text>
+        </View>
+
+        <View style={GuiaPlantadoStyles.plantInfoContainer}>
+          <Text style={GuiaPlantadoStyles.infoLabel}>Detalles: </Text>
+          <Text style={GuiaPlantadoStyles.infoText}> Preparar la maceta y el sustrato</Text>
+        </View>
+
+        <View style={GuiaPlantadoStyles.plantInfoContainer}>
+          <Text style={GuiaPlantadoStyles.infoLabel}>Estado de la Planta: </Text>
+          <Text style={GuiaPlantadoStyles.infoText}> Preparar la maceta y el sustrato</Text>
+        </View>
+
+        <View style={GuiaPlantadoStyles.plantInfoContainer}>
+          <Button color={'#5ac15d'} title='¿Qué hago ahora?'/>
+        </View>
+
+      </View>
     </View>
     );
 }
