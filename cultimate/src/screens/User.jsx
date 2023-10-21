d471bd4e9c3fb7a0517ec713a7a1a46b99303eef
Yavae <98843718+Yavae@users.noreply.github.com>
2023-10-21 21:54:24 +0200

    cambios en UserProfile screen

--- cultimate/src/screens/User.jsx
+++ cultimate/src/screens/User.jsx
@@ -1,70 +1,89 @@
 import React, { useState } from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet} from 'react-native';
+import { View, Text, Image, TouchableOpacity, StyleSheet, ScrollView} from 'react-native';
+import GridIconos from '../components/GridIconos';
+
 
 const User = () => {
-  const [userPhoto, setUserPhoto] = useState(); // Reemplaza 'URL_DE_LA_FOTO' con la URL real de la foto del usuario.
+  const elements = [
+    { id: 1, title: 'Logro 1', imageSource: require('../../assets/Fresa.png') },
+    { id: 2, title: 'Logro 2', imageSource: require('../../assets/mora.png') },
+    { id: 3, title: 'Logro 3', imageSource: require('../../assets/tomate.png') },
+    { id: 4, title: 'Logro 4', imageSource: require('../../assets/pepino.png') },
+    { id: 5, title: 'Logro 5', imageSource: require('../../assets/pimientos.png') },
+    { id: 6, title: 'Logro 6', imageSource: require('../../assets/Fresa.png') },
+    { id: 7, title: 'Logro 7', imageSource: require('../../assets/mora.png') },
+    // Puedes agregar más logros aquí
+  ];
 
-  const handleChangePhoto = () => {
-    // Implementa la lógica para cambiar la foto del usuario aquí.
-    
-  };
 
   return (
     <View style={styles.container}>
-      {/* Foto del usuario y botón para cambiarla */}
-      <View style={styles.header}>
-        <Image
-          style={styles.userPhoto}
-          source={require("../../assets/termometro.png")}
-        />
-        <TouchableOpacity onPress={handleChangePhoto} style={styles.changePhotoButton}>
-          <Text>Cambiar Foto</Text>
-        </TouchableOpacity>
+      <View style={styles.centeredView}>
+        <View style={styles.profileImageBackground}>
+          <TouchableOpacity>
+            <Image
+              source={require('../../assets/Fresa.png')} // Ruta de la imagen
+              style={styles.profileImage}
+            />
+          </TouchableOpacity>
+        </View>
+        <Text style={styles.userName}>Nombre del Usuario</Text>
       </View>
-
-      {/* Nombre de usuario */}
-      <Text style={styles.username}>Nombre de Usuario</Text>
-
-      {/* Título de Plantas Favoritas */}
-      <Text style={styles.favoritesTitle}>Plantas Favoritas</Text>
-      {/* Implementa la lógica para mostrar la lista de plantas favoritas aquí. */}
+      <View style={styles.centeredView}>
+        <View style={styles.containerTitle}>
+          <Text style={styles.title}>Logros</Text>
+        </View>
+        <GridIconos elements={elements}/>
+      </View>
     </View>
-    );
-  };
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      paddingHorizontal: 20,
-      paddingTop: 20,
-    },
-    header: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'flex-end',
-      width: '100%',
-    },
-    userPhoto: {
-      width: 80,
-      height: 80,
-      borderRadius: 40,
-    },
-    changePhotoButton: {
-      marginLeft: 10,
-      backgroundColor: '#DDDDDD',
-      padding: 5,
-      borderRadius: 5,
-    },
-    username: {
-      marginTop: 10,
-      fontSize: 18,
-    },
-    favoritesTitle: {
-      fontSize: 24,
-      fontWeight: 'bold',
-      marginTop: 20,
-    },
-  });
-  
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F0F0F0',
+    padding: 10,
+  },
+  centeredView: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+    backgroundColor: 'white',
+    padding: 10,
+  },
+  profileImageBackground: {
+    borderRadius: 100,
+    backgroundColor: 'lightgrey',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'black',
+    borderWidth: 3,
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+    margin: 8,
+  },
+  userName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginVertical: 15,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    textAlign: 'left',
+  },
+  containerTitle: {
+    // flexDirection: 'column', // Puedes quitar esta línea si quieres que sea una columna
+    textAlign: 'left',
+    width: '100%',
+    paddingTop: 10,
+  },
+  // ... Otros estilos
+});
+
 export default User;
