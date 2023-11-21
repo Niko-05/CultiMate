e6d311a7e90524898f08028fd68eef83367c4539
Annabdoan <71780695+Annabdoan@users.noreply.github.com>
2023-11-21 10:51:40 +0100

    feat: frequent question page

--- cultimate/config.jsx
+++ cultimate/config.jsx
@@ -1,5 +1,5 @@
 // remember to change this to your ip address (localhost)
-const ip = "10.236.39.205";
+const ip = "158.42.191.198";
 const port = "3000";
 const API = "http://" + ip + ":" + port;
 
