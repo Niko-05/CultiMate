fd9a7e8e61a904767b3432b25636c9553382263b
Niko-05 <nicolasmz0511@gmail.com>
2023-11-22 10:55:36 +0100

    Tienda y producto creadas

--- cultimate/src/navigation/Ventanas.jsx
+++ cultimate/src/navigation/Ventanas.jsx
@@ -9,6 +9,7 @@ import ConfigScreen from "../screens/ConfigScreen";
 import AccountSettingsScreen from "../screens/AccountSettings";
 import ProfilePicture from "../screens/ProfilePicture";
 import PreguntasFrecuentes from "../screens/PreguntasFrecuentes";
+import Producto from "../screens/tienda/producto";
 
 const Stack = createStackNavigator();
 const Ventanas = () => {
@@ -37,6 +38,7 @@ const Ventanas = () => {
       <Stack.Screen name="GuiaPlantado" component={GuiaPlantado} />
       <Stack.Screen name="GuiasPlantado" component={GuiasPlantado} />
       <Stack.Screen name="PreguntasFrecuentes" component={PreguntasFrecuentes} />
+      <Stack.Screen name="Producto" component={Producto} />
     </Stack.Navigator>
   );
 };
