39ff7d8833f3bca8aa4e1806fc513af31ed8ee74
CamarillaGuanxi <valentinodominguezrabanal@gmail.com>
2023-11-22 21:20:55 +0100

    Favoritos(sin boton)

--- cultimate/src/screens/NewPlant.jsx
+++ cultimate/src/screens/NewPlant.jsx
@@ -2,25 +2,87 @@ import React, { useState, useEffect } from "react";
 import { SafeAreaView, Text, Pressable, Alert } from "react-native";
 import ListaPlanta from "../components/ListaPlanta";
 import { SuggestionModal } from "../components/SuggestionModal";
+import * as SecureStore from "expo-secure-store";
+import config from "../../config";
 
-import config from '../../config';
 
+
+  
+
+  
 const NewPlant = ({ navigation }) => {
   const [data, setData] = useState([]);
+  const [favList, setFav] = useState([]);
+  const getUserInfo = async () => {
+    try {
+      // This is the way to access the token
+      const token = await SecureStore.getItemAsync("accesstoken");
+      const api_call = await fetch(`${config.API}/user/`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+  
+      if (!api_call.ok) {
+        // Handle non-OK response status
+        Alert.alert(
+          "API error",
+          `Failed to fetch user data. Status: ${api_call.status}`
+        );
+        return;
+      }
+  
+      const result = await api_call.json();
+      return result;
+    } catch (e) {
+      console.error(e);
+      Alert.alert("Network error");
+    }
+  };
 
+  const cambioFav = async ()=>{
+    try{
+      const user = await getUserInfo();
+      const token = await SecureStore.getItemAsync("accesstoken");
+      const api_call = await fetch(
+      `${config.API}/fav/favoritos?id=${encodeURIComponent(
+        user.id
+      )}`,
+      { method: "GET" }
+    );
+   const result = await api_call.json();
+   setFav(result);
+
+    if (!api_call.ok) {
+      // Handle non-OK response status
+      Alert.alert(
+        "API error",
+        `Failed to fetch user data. Status: ${api_call.status}`
+      );
+    }
+  
+   
+  } catch (e) {
+    console.error(e);
+    Alert.alert("Network error");
+  }
+  };
   const setFullList = async () => {
     try {
       const api_call = await fetch(`${config.API}/planta`);
       const result = await api_call.json();
       setData(result);
-      console.log(result);
     } catch (e) {
       Alert.alert('Problema de red', 'No se ha podido mostrar el listado de plantas debido a un problema de red.');
     }
   }
 
+  
   useEffect(() => {
     setFullList();
+    cambioFav();
   }, []);
 
   const [isModalVisible, setModalVisible] = useState(false);
@@ -33,7 +95,7 @@ const NewPlant = ({ navigation }) => {
     <SafeAreaView
       style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
     >
-      <ListaPlanta data={data} navigation={navigation} />
+      <ListaPlanta data={data} favLista={favList} navigation={navigation} />
       <Pressable
         style={{
           backgroundColor: "lightgreen",
