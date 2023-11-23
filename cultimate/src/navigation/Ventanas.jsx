6a8860a0e3269b9dd7c70f2f438c70268fb30d79
Niko-05 <nicolasmz0511@gmail.com>
2023-11-23 11:28:32 +0100

    Tienda completada y terminada

--- cultimate/src/navigation/Ventanas.jsx
+++ cultimate/src/navigation/Ventanas.jsx
@@ -9,7 +9,6 @@ import ConfigScreen from "../screens/ConfigScreen";
 import AccountSettingsScreen from "../screens/AccountSettings";
 import ProfilePicture from "../screens/ProfilePicture";
 import PreguntasFrecuentes from "../screens/PreguntasFrecuentes";
-import Producto from "../screens/tienda/producto";
 
 const Stack = createStackNavigator();
 const Ventanas = () => {
@@ -38,7 +37,6 @@ const Ventanas = () => {
       <Stack.Screen name="GuiaPlantado" component={GuiaPlantado} />
       <Stack.Screen name="GuiasPlantado" component={GuiasPlantado} />
       <Stack.Screen name="PreguntasFrecuentes" component={PreguntasFrecuentes} />
-      <Stack.Screen name="Producto" component={Producto} />
     </Stack.Navigator>
   );
 };
