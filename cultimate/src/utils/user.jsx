d991dc9f1e5201784350707493fa03a52e4f4c57
XuclaFort <127497816+XuclaFort@users.noreply.github.com>
2023-11-23 18:11:50 +0100

    Imagenes añadidas

--- cultimate/src/utils/user.jsx
+++ cultimate/src/utils/user.jsx
@@ -7,18 +7,22 @@ export const checkValidEmail = (email) => {
 
 export const profilePictures = [
   {
-    id: 2,
-    title: "Strawberry",
-    imageSource: require("../../assets/Fresa.png"),
-  },
-  { id: 3, title: "Grape", imageSource: require("../../assets/mora.png") },
-  {
     id: 1,
     title: "Tomato",
     imageSource: require("../../assets/tomate.png"),
   },
   {
     id: 2,
+    title: "Strawberry",
+    imageSource: require("../../assets/Fresa.png"),
+  },
+  { 
+    id: 3, 
+    title: "Grape", 
+    imageSource: require("../../assets/mora.png") 
+  },
+  {
+    id: 4,
     title: "Cucumber",
     imageSource: require("../../assets/pepino.png"),
   },
@@ -27,7 +31,61 @@ export const profilePictures = [
     title: "Pepper",
     imageSource: require("../../assets/pimientos.png"),
   },
-  { id: 6, title: "Lemon", imageSource: require("../../assets/limon.png") },
+  { 
+    id: 6, 
+    title: "Lechuga", 
+    imageSource: require("../../assets/lechuga.png") 
+  },
+  {
+    id: 7,
+    title: "Cebolla",
+    imageSource: require("../../assets/cebolla.png"),
+  },
+  {
+    id: 8,
+    title: "Zanahoria",
+    imageSource: require("../../assets/zanahoria.png"),
+  },
+  {
+    id: 9,
+    title: "Espinaca",
+    imageSource: require("../../assets/espinaca.png"),
+  },
+  {
+    id: 10,
+    title: "Calabacín",
+    imageSource: require("../../assets/calabacin.png"),
+  },
+  {
+    id: 11,
+    title: "Frambuesa",
+    imageSource: require("../../assets/frambuesa.png"),
+  },
+  {
+    id: 12,
+    title: "Brócoli",
+    imageSource: require("../../assets/brocoli.png"),
+  },
+  {
+    id: 13,
+    title: "Limon",
+    imageSource: require("../../assets/limon.png"),
+  },
+  {
+    id: 14,
+    title: "Tomate cherry",
+    imageSource: require("../../assets/cherry.png"),
+  },
+  {
+    id: 15,
+    title: "Albahaca",
+    imageSource: require("../../assets/albahaca.png"),
+  },
+  {
+    id: 16,
+    title: "Menta",
+    imageSource: require("../../assets/menta.png"),
+  },
 ];
 
 export const getProfilePictureSource = (id) => {
