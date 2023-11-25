8a6926e314adcc31c2eccfbeef139ddc8e1daf6a
Yavae <98843718+Yavae@users.noreply.github.com>
2023-11-25 13:31:19 +0100

    procedo a toquetear dependencias

--- cultimate/src/screens/ConfigScreen.jsx
+++ cultimate/src/screens/ConfigScreen.jsx
@@ -1,33 +1,71 @@
-import React, { useState } from "react";
-import { View, Text, Switch, TouchableOpacity } from "react-native";
+import React, { useState,useEffect } from "react";
+import { View, Text, Switch, TouchableOpacity, StyleSheet } from "react-native";
+import AsyncStorage from "@react-native-community/async-storage";
 import { Picker } from "@react-native-picker/picker";
 
+
 const ConfigScreen = ({ navigation }) => {
-  const [idiomaSeleccionado, setIdiomaSeleccionado] = useState("Español");
+  const [idiomaSeleccionado, setIdiomaSeleccionado] = useState('Español');
   const [notificacionesActivadas, setNotificacionesActivadas] = useState(true);
   const [modoOscuroActivado, setModoOscuroActivado] = useState(false);
+  const [sonidoActivado, setSonidoActivado] = useState(true);
 
-  const handleProfileSettings = () => {
-    navigation.navigate("Account settings");
+  const saveConfiguration = async () => {
+    try {
+      const config = {
+        idiomaSeleccionado,
+        notificacionesActivadas,
+        sonidoActivado,
+        modoOscuroActivado,
+      };
+
+      await AsyncStorage.setItem('config', JSON.stringify(config));
+      console.log('Configuración guardada:', config);
+    } catch (error) {
+      console.error('Error al guardar la configuración:', error);
+    }
+  };
+
+  const loadConfiguration = async () => {
+    try {
+      const configString = await AsyncStorage.getItem('config');
+      if (configString) {
+        const config = JSON.parse(configString);
+        setIdiomaSeleccionado(config.idiomaSeleccionado);
+        setNotificacionesActivadas(config.notificacionesActivadas);
+        setSonidoActivado(config.sonidoActivado);
+        setModoOscuroActivado(config.modoOscuroActivado);
+      }
+    } catch (error) {
+      console.error('Error al cargar la configuración:', error);
+    }
+  };
+
+  useEffect(() => {
+    // Cargar la configuración al abrir la pantalla
+    loadConfiguration();
+  }, []);
+
+  const handleProfileSettings = async () => {
+    await saveConfiguration();
+    navigation.navigate('Account settings');
   };
 
   return (
-    <View className="flex-1 flex-col items-center">
-      <View className="w-80 mt-6">
-        <Text className="text-center font-bold">Language:</Text>
-
+    <View style={[styles.container, modoOscuroActivado && styles.modoOscuro]}>
+      <View style={styles.languageContainer}>
+        <Text style={styles.languageText}>Language:</Text>
         <Picker
           selectedValue={idiomaSeleccionado}
           onValueChange={(valor) => setIdiomaSeleccionado(valor)}
         >
           <Picker.Item label="Español" value="Español" />
           <Picker.Item label="English" value="English" />
-          <Picker.Item label="Francés" value="Francés" />
         </Picker>
       </View>
 
-      <View className="flex-row items-center mb-4 space-x-2">
-        <Text className="font-bold">Notifications:</Text>
+      <View style={styles.notificationRow}>
+        <Text style={styles.boldText}>Notifications:</Text>
         <Switch
           value={notificacionesActivadas}
           onValueChange={() =>
@@ -35,19 +73,31 @@ const ConfigScreen = ({ navigation }) => {
           }
         />
       </View>
-      <View className="flex-row items-center mb-4 space-x-2">
-        <Text className="font-bold">Dark mode:</Text>
+
+      <View style={styles.notificationRow}>
+        <Text style={styles.boldText}>Sonido:</Text>
+        <Switch
+          value={sonidoActivado}
+          onValueChange={() =>
+            setSonidoActivado(!sonidoActivado)
+          }
+        />
+      </View>
+
+      <View style={styles.notificationRow}>
+        <Text style={styles.boldText}>Dark mode:</Text>
         <Switch
           value={modoOscuroActivado}
           onValueChange={() => setModoOscuroActivado(!modoOscuroActivado)}
         />
       </View>
-      <View>
+
+      <View style={styles.buttonContainer}>
         <TouchableOpacity
-          className="bg-slate-400 p-4 rounded-md"
+          style={styles.button}
           onPress={() => handleProfileSettings()}
         >
-          <Text className="font-bold text-white">Account settings</Text>
+          <Text style={styles.buttonText}>Account settings</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -55,3 +105,45 @@ const ConfigScreen = ({ navigation }) => {
 };
 
 export default ConfigScreen;
+
+
+const styles = {
+
+  modoOscuro: {
+    backgroundColor: modoOscuroActivado ? '#000' : '#fff',
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  languageContainer: {
+    width: 80,
+    marginTop: 6,
+  },
+  languageText: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  notificationRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 4,
+    marginHorizontal: 10,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  buttonContainer: {
+    marginTop: 10,
+  },
+  button: {
+    backgroundColor: '#4B5563', 
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    color: 'white',
+  },
+};
---
+++ cultimate/src/styles/StylesModos.js
@@ -0,0 +1,32 @@
+
+const darkThemeStyles = {
+    container: {
+        backgroundColor: '#1a1a1a',
+        color: '#ffffff',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        color: '#ffffff',
+        fontSize: '16px',
+        fontWeight: 'bold',
+    },
+};
+
+const lightThemeStyles = {
+    container: {
+        backgroundColor: '#ffffff',
+        color: '#000000',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        color: '#000000',
+        fontSize: '16px',
+        fontWeight: 'bold',
+    },
+};
+
+export { darkThemeStyles, lightThemeStyles };
