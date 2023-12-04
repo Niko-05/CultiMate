342d6f44df4b45a2d0305ea9ac87baa7c99661ed
Oscar Timm Marcussen <oscar.marcussen@gmail.com>
2023-12-04 19:10:27 +0100

    feat: fix design on loginscreen

--- cultimate/src/screens/login/LoginScreen.jsx
+++ cultimate/src/screens/login/LoginScreen.jsx
@@ -1,13 +1,26 @@
-import React, { useState } from "react";
-import { View, Text, TouchableOpacity, Alert, Image, StyleSheet} from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Alert,
+  Image,
+  StyleSheet,
+  TextInput,
+  ActivityIndicator,
+} from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import TextInputLogin from "../../components/TextInputLogin";
 import { loginUser } from "../../api/user";
+import EStyleSheet from "react-native-extended-stylesheet";
+import Logo from "../../../assets/login/logo_new.svg";
+import * as Font from "expo-font";
 
 const LoginScreen = ({ navigation }) => {
   const insets = useSafeAreaInsets();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [fontsLoaded, setFontsLoaded] = useState(false);
 
   const getUser = async () => {
     if (email === "" || password === "") {
@@ -20,66 +33,266 @@ const LoginScreen = ({ navigation }) => {
     }
   };
 
+  useEffect(() => {
+    async function loadFont() {
+      await Font.loadAsync({
+        "Integral CF": require("../../../assets/fonts/Integral_CF_medium.otf"),
+        Inter: require("../../../assets/fonts/Inter.ttf"),
+      });
+      setFontsLoaded(true);
+    }
+    loadFont();
+  }, []);
+
+  //   return (
+  //     <View className="flex-1">
+  //       <View
+  //         style={{ marginTop: insets.top, marginBottom: insets.bottom }}
+  //         className="flex-1 justify-center mx-8"
+  //       >
+  //         <View style={{
+  //           alignItems: 'center',
+  //           marginBottom: 30
+  //           }}>
+  //           <Image
+  //             style={styles.centeredImage}
+  //             source={require("../../../assets/icono-cultimate.png")}
+  //           />
+  //         </View>
+  //         <View className="space-y-2 mb-6">
+  //           <Text className="font-bold text-2xl">Sign In</Text>
+  //           <Text className="text-sm text-gray-500">
+  //             Hi there! Nice to see you again.
+  //           </Text>
+  //         </View>
+  //         <View className="mb-4">
+  //           <TextInputLogin
+  //             label={"E-mail"}
+  //             placeholder={"E-mail"}
+  //             inputMode={"email"}
+  //             onChangeText={setEmail}
+  //           />
+  //           <TextInputLogin
+  //             label={"Password"}
+  //             placeholder={"Password"}
+  //             inputMode={"text"}
+  //             onChangeText={setPassword}
+  //             secureTextEntry
+  //           />
+  //         </View>
+  //         <TouchableOpacity
+  //           className="items-center bg-green-700 py-3 rounded-lg"
+  //           onPress={() => getUser()}
+  //         >
+  //           <Text className="text-white font-bold text-lg">Sign in</Text>
+  //         </TouchableOpacity>
+  //         <View className="flex-row my-8 justify-center space-x-1">
+  //           <Text className="text-gray-500">Not registered?</Text>
+  //           <TouchableOpacity
+  //             className=""
+  //             onPress={() => navigation.navigate("RegisterScreen")}
+  //           >
+  //             <Text className="text-green-700 font-bold">Sign up</Text>
+  //           </TouchableOpacity>
+  //         </View>
+  //       </View>
+  //     </View>
+  //   );
+  // };
+
+  // const styles = StyleSheet.create({
+  //   centeredImage: {
+  //     width: 270, // Adjust the size as needed
+  //     height: 270, // Adjust the size as needed
+  //   },
+  // });
+
+  if (!fontsLoaded) {
+    return (
+      <View className="items-center justify-center">
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
   return (
-    <View className="flex-1">
-      <View
-        style={{ marginTop: insets.top, marginBottom: insets.bottom }}
-        className="flex-1 justify-center mx-8"
-      >
-        <View style={{
-          alignItems: 'center',
-          marginBottom: 30
-          }}>
-          <Image
-            style={styles.centeredImage}
-            source={require("../../../assets/icono-cultimate.png")}
-          />
+    <View className="flex-1 bg-white">
+      <Image
+        style={styles.pumpkin}
+        source={require("../../../assets/login/pumpkin.png")}
+      />
+      <Image
+        style={styles.broccoli}
+        source={require("../../../assets/login/broccoli.png")}
+      />
+      <Image
+        style={styles.tomato}
+        source={require("../../../assets/login/tomato.png")}
+      />
+      <Image
+        style={styles.carrot}
+        source={require("../../../assets/login/carrot.png")}
+      />
+      <Image
+        style={styles.pepper}
+        source={require("../../../assets/login/pepper.png")}
+      />
+      <Image
+        style={styles.lemon}
+        source={require("../../../assets/login/lemon.png")}
+      />
+      <Image
+        style={styles.lettuce}
+        source={require("../../../assets/login/lettuce.png")}
+      />
+      <View className="flex-1 items-center justify-center">
+        <View>
+          <Logo />
         </View>
-        <View className="space-y-2 mb-6">
-          <Text className="font-bold text-2xl">Sign In</Text>
-          <Text className="text-sm text-gray-500">
-            Hi there! Nice to see you again.
-          </Text>
-        </View>
-        <View className="mb-4">
-          <TextInputLogin
-            label={"E-mail"}
-            placeholder={"E-mail"}
-            inputMode={"email"}
+      </View>
+      <View className="flex-1 items-center">
+        <Text style={styles.welcomeText}>BIENVENIDO</Text>
+        <View className="space-y-[30] my-[35]">
+          <TextInput
+            style={styles.input}
+            placeholder="Correo electrónico"
             onChangeText={setEmail}
+            inputMode="email"
           />
-          <TextInputLogin
-            label={"Password"}
-            placeholder={"Password"}
-            inputMode={"text"}
+          <TextInput
+            style={styles.input}
+            placeholder="Contraseña"
             onChangeText={setPassword}
+            inputMode="text"
             secureTextEntry
           />
-        </View>
-        <TouchableOpacity
-          className="items-center bg-green-700 py-3 rounded-lg"
-          onPress={() => getUser()}
-        >
-          <Text className="text-white font-bold text-lg">Sign in</Text>
-        </TouchableOpacity>
-        <View className="flex-row my-8 justify-center space-x-1">
-          <Text className="text-gray-500">Not registered?</Text>
           <TouchableOpacity
-            className=""
-            onPress={() => navigation.navigate("RegisterScreen")}
+            style={styles.loginButton}
+            onPress={() => getUser()}
           >
-            <Text className="text-green-700 font-bold">Sign up</Text>
+            <Text style={styles.loginText}>Login</Text>
           </TouchableOpacity>
         </View>
+        <Text style={styles.registerText}>
+          ¿Quieres crear tu propio huerto?
+        </Text>
+        <TouchableOpacity
+          style={styles.subscribeButton}
+          onPress={() => navigation.navigate("RegisterScreen")}
+        >
+          <Text style={styles.subscribeButtonText}>Suscríbete aquí</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  centeredImage: {
-    width: 270, // Adjust the size as needed
-    height: 270, // Adjust the size as needed
+  pumpkin: {
+    width: 130,
+    height: 137,
+    top: -67,
+    right: -30,
+    position: "absolute",
+  },
+  broccoli: {
+    width: 135,
+    height: 151,
+    top: -26,
+    left: -32,
+    position: "absolute",
+  },
+  tomato: {
+    width: 154,
+    height: 129.2,
+    transform: [{ rotate: "75.26deg" }],
+    left: -53,
+    bottom: "47%",
+    position: "absolute",
+  },
+  carrot: {
+    width: 95,
+    height: 102.3,
+    right: 15,
+    bottom: "52%",
+    position: "absolute",
+  },
+  pepper: {
+    width: 162,
+    height: 147,
+    right: -32,
+    bottom: "19.2%",
+    position: "absolute",
+  },
+  lemon: {
+    width: 105.645,
+    height: 125,
+    transform: [{ rotate: "-35.159deg" }],
+    left: -12,
+    bottom: "6.2%",
+    position: "absolute",
+  },
+  lettuce: {
+    width: 119,
+    height: 105,
+    right: 73,
+    bottom: -29,
+    position: "absolute",
+  },
+  welcomeText: {
+    color: "black",
+    fontFamily: "Integral CF",
+    fontSize: 18,
+  },
+  input: {
+    borderRadius: 15,
+    borderColor: "black",
+    padding: 22,
+    borderWidth: 1,
+    backgroundColor: "white",
+    width: 244,
+    color: "black",
+    fontFamily: "Inter",
+    fontSize: 14,
+    fontWeight: 500,
+  },
+  registerText: {
+    color: "#939393",
+    fontFamily: "Inter",
+    fontSize: 12,
+    fontWeight: 400,
+  },
+  loginButton: {
+    alignItems: "center",
+    paddingHorizontal: 22,
+    paddingVertical: 8,
+    borderRadius: 30,
+    borderColor: "black",
+    padding: 22,
+    borderWidth: 1,
+    backgroundColor: "#09873D",
+    marginTop: 15,
+  },
+  loginText: {
+    color: "white",
+    fontFamily: "Inter",
+    fontSize: 15,
+    fontWeight: 400,
+  },
+  subscribeButton: {
+    paddingHorizontal: 22,
+    paddingVertical: 8,
+    borderRadius: 30,
+    borderColor: "black",
+    padding: 22,
+    borderWidth: 1,
+    backgroundColor: "white",
+    marginTop: 15,
+  },
+  subscribeButtonText: {
+    color: "black",
+    fontFamily: "Inter",
+    fontSize: 15,
+    fontWeight: 400,
   },
 });
 
--- cultimate/src/screens/login/RegisterScreen.jsx
+++ cultimate/src/screens/login/RegisterScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity, Alert } from "react-native";
+import { View, Text, TouchableOpacity, Alert, StyleSheet } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import TextInputLogin from "../../components/TextInputLogin";
 import CheckIcon from "../../../assets/check-solid.svg";
@@ -58,73 +58,93 @@ const RegisterScreen = ({ navigation }) => {
     }
   };
 
+  // return (
+  //   <View className="flex-1">
+  //     <View
+  //       style={{ marginTop: insets.top, marginBottom: insets.bottom }}
+  //       className="flex-1 justify-center mx-8"
+  //     >
+  //       <View className="space-y-2 mb-6">
+  //         <Text className="font-bold text-2xl">Sign Up</Text>
+  //       </View>
+  //       <View className="mb-4">
+  //         <TextInputLogin
+  //           label={"Username"}
+  //           placeholder={"Username"}
+  //           onChangeText={setUsername}
+  //         />
+  //         <TextInputLogin
+  //           label={"E-mail"}
+  //           placeholder={"E-mail"}
+  //           onChangeText={setEmail}
+  //         />
+  //         <TextInputLogin
+  //           label={"Password"}
+  //           placeholder={"Password"}
+  //           onChangeText={setPassword}
+  //           secureTextEntry
+  //         />
+  //         <TextInputLogin
+  //           label={"Confirm password"}
+  //           placeholder={"Password"}
+  //           onChangeText={setConfirmPassword}
+  //           secureTextEntry
+  //         />
+  //       </View>
+  //       <View className="mb-4 flex-row space-x-2">
+  //         <TouchableOpacity
+  //           accessibilityRole={"checkbox"}
+  //           checked={check}
+  //           onPress={() => setCheck(!check)}
+  //           className={classnames(
+  //             "w-7 h-7 border-2 rounded-md p-0.5",
+  //             check ? "border-green-700 bg-green-700" : "border-gray-400"
+  //           )}
+  //         >
+  //           {check ? <CheckIcon fill="white" /> : <CheckIcon fill="gray" />}
+  //         </TouchableOpacity>
+  //         <Text className="flex-1" numberOfLines={2}>
+  //           I agree to the Terms of Services and Privacy Policy
+  //         </Text>
+  //       </View>
+  //       <TouchableOpacity
+  //         className="items-center bg-green-700 py-3 rounded-lg"
+  //         onPress={() => setUser()}
+  //       >
+  //         <Text className="text-white font-bold text-lg">Continue</Text>
+  //       </TouchableOpacity>
+  //       <View className="flex-row my-8 justify-center space-x-1">
+  //         <Text className="text-gray-500">Have an account?</Text>
+  //         <TouchableOpacity
+  //           className=""
+  //           onPress={() => navigation.navigate("LoginScreen")}
+  //         >
+  //           <Text className="text-green-700 font-bold">Sign In</Text>
+  //         </TouchableOpacity>
+  //       </View>
+  //     </View>
+  //   </View>
+  // );
   return (
-    <View className="flex-1">
-      <View
-        style={{ marginTop: insets.top, marginBottom: insets.bottom }}
-        className="flex-1 justify-center mx-8"
-      >
-        <View className="space-y-2 mb-6">
-          <Text className="font-bold text-2xl">Sign Up</Text>
-        </View>
-        <View className="mb-4">
-          <TextInputLogin
-            label={"Username"}
-            placeholder={"Username"}
-            onChangeText={setUsername}
-          />
-          <TextInputLogin
-            label={"E-mail"}
-            placeholder={"E-mail"}
-            onChangeText={setEmail}
-          />
-          <TextInputLogin
-            label={"Password"}
-            placeholder={"Password"}
-            onChangeText={setPassword}
-            secureTextEntry
-          />
-          <TextInputLogin
-            label={"Confirm password"}
-            placeholder={"Password"}
-            onChangeText={setConfirmPassword}
-            secureTextEntry
-          />
-        </View>
-        <View className="mb-4 flex-row space-x-2">
-          <TouchableOpacity
-            accessibilityRole={"checkbox"}
-            checked={check}
-            onPress={() => setCheck(!check)}
-            className={classnames(
-              "w-7 h-7 border-2 rounded-md p-0.5",
-              check ? "border-green-700 bg-green-700" : "border-gray-400"
-            )}
-          >
-            {check ? <CheckIcon fill="white" /> : <CheckIcon fill="gray" />}
-          </TouchableOpacity>
-          <Text className="flex-1" numberOfLines={2}>
-            I agree to the Terms of Services and Privacy Policy
-          </Text>
-        </View>
-        <TouchableOpacity
-          className="items-center bg-green-700 py-3 rounded-lg"
-          onPress={() => setUser()}
-        >
-          <Text className="text-white font-bold text-lg">Continue</Text>
-        </TouchableOpacity>
-        <View className="flex-row my-8 justify-center space-x-1">
-          <Text className="text-gray-500">Have an account?</Text>
-          <TouchableOpacity
-            className=""
-            onPress={() => navigation.navigate("LoginScreen")}
-          >
-            <Text className="text-green-700 font-bold">Sign In</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+    <View style={styles.bg}>
+      <View style={styles.modal}></View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  bg: {
+    backgroundColor: "#09873D",
+    flex: 1,
+  },
+  modal: {
+    backgroundColor: "#fff",
+    height: "50%",
+    position: "absolute",
+    bottom: 0,
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+  },
+});
 
 export default RegisterScreen;
