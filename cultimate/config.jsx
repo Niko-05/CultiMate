da04bb1aef6efdcfb21ada5dce49ab06e839c980
Yavae <98843718+Yavae@users.noreply.github.com>
2023-12-06 16:01:51 +0100

    port

--- cultimate/config.jsx
+++ cultimate/config.jsx
@@ -1,6 +1,6 @@
 // remember to change this to your ip address (localhost)
 const ip = "192.168.18.14";
-const port = "4000";
+const port = "3000";
 const API = "http://" + ip + ":" + port;
 
 export default {
