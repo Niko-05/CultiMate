048bddbef2ed2f21d885d9c862334fad39d9a976
Anna Doan <71780695+Annabdoan@users.noreply.github.com>
2023-12-12 15:33:53 +0100

    16 mejorar diseno preguntas frecuentes (#21)
    * fix: accordian style

--- cultimate/src/screens/PreguntasFrecuentes.jsx
+++ cultimate/src/screens/PreguntasFrecuentes.jsx
@@ -1,60 +1,141 @@
-import React from "react";
-import { StyleSheet, View, Text, ScrollView } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View, Text, TouchableOpacity, Image } from "react-native";
+import Collapsible from "react-native-collapsible";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 
 const PreguntasFrecuentes = () => {
-    const preguntas = [
-        {
-            pregunta: "¿Cómo puedo crear una cuenta en CultiMate?",
-            respuesta:
-                'Para crear una cuenta en CultiMate, simplemente haz clic en el botón "Registrarse" en la página de inicio y sigue las instrucciones.',
-        },
-        {
-            pregunta: "¿Cómo puedo agregar una planta a mi lista de seguimiento?",
-            respuesta:
-                'Para agregar una planta a tu lista de seguimiento, busca la planta en la página de búsqueda y haz clic en el botón "Agregar a mi lista".',
-        },
-        {
-            pregunta: "¿Cómo puedo eliminar una planta de mi lista de seguimiento?",
-            respuesta:
-                'Para eliminar una planta de tu lista de seguimiento, ve a la página de tu lista de seguimiento y haz clic en el botón "Eliminar" junto a la planta que deseas eliminar.',
-        },
-    ];
+  const preguntas = [
+    {
+      pregunta:
+        "¿Dónde puedo conseguir los fertilizantes necesarios para el crecimiento de mis plantas?",
+      respuesta:
+        "Puedes comprar todos los productos necesarios en el apartado de tienda de nuestra app.",
+    },
+    {
+      pregunta: "¿Cada cuánto tengo que regar mis plantas?",
+      respuesta:
+        "Para esto te recomendamos consultar la información específica de cada planta, ya que cada una tiene unas necesidades específicas.",
+    },
+    {
+      pregunta:
+        "¿Dónde puedo conseguir los fertilizantes necesarios para el crecimiento de mis plantas?",
+      respuesta:
+        "Puedes comprar todos los productos necesarios en el apartado de tienda de nuestra app.",
+    },
+  ];
 
+  const [activeSection, setActiveSection] = useState(undefined);
+
+  const toggleSection = (index) => {
+    setActiveSection(activeSection === index ? undefined : index);
+  };
+
+  const renderHeader = (section, index) => {
     return (
-        <ScrollView style={styles.container}>
-            <Text style={styles.title}>Preguntas frecuentes</Text>
-            {preguntas.map((pregunta, index) => (
-                <View key={index} style={styles.preguntaContainer}>
-                    <Text style={styles.pregunta}>{pregunta.pregunta}</Text>
-                    <Text style={styles.respuesta}>{pregunta.respuesta}</Text>
-                </View>
-            ))}
-        </ScrollView>
+      <TouchableOpacity
+        style={styles.preguntaContainer}
+        onPress={() => toggleSection(index)}
+      >
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Text style={[styles.pregunta, { marginRight: 10 }]}>
+            {section.pregunta}
+          </Text>
+          <MaterialIcons
+            name={
+              activeSection === index
+                ? "keyboard-arrow-up"
+                : "keyboard-arrow-down"
+            }
+            size={24}
+          />
+        </View>
+      </TouchableOpacity>
     );
+  };
+
+  const renderContent = (section, index) => {
+    return (
+      <View style={styles.respuestaContainer}>
+        <Collapsible collapsed={activeSection !== index}>
+          <Text style={styles.respuesta}>{section.respuesta}</Text>
+        </Collapsible>
+      </View>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.top}>
+        <Image
+          style={styles.imageBackground}
+          source={require("../../assets/ESPINACAS_LINEA_BLANCA.png")}
+        />
+        <Text
+          style={{
+            color: "white",
+            fontSize: 24,
+            textAlign: "center",
+            fontFamily: "Integral CF",
+            lineHeight: 28,
+          }}
+        >
+          Preguntas Frecuentes
+        </Text>
+      </View>
+      <View style={styles.modal}>
+        {preguntas.map((section, index) => (
+          <View key={index}>
+            {renderHeader(section, index)}
+            {renderContent(section, index)}
+          </View>
+        ))}
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 20,
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: "bold",
-        marginBottom: 20,
-    },
-    preguntaContainer: {
-        marginBottom: 20,
-    },
-    pregunta: {
-        fontSize: 18,
-        fontWeight: "bold",
-        marginBottom: 10,
-    },
-    respuesta: {
-        fontSize: 16,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#09873D",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  top: {
+    flex: 0.3,
+    flexDirection: "column",
+    width: "100%",
+    alignItems: "center",
+  },
+  modal: {
+    flex: 0.7,
+    backgroundColor: "white",
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+    alignItems: "center",
+  },
+  preguntaContainer: {
+    padding: 10,
+    marginBottom: 10,
+    justifyContent: "center",
+    top: 10,
+  },
+  pregunta: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  respuestaContainer: {
+    padding: 10,
+  },
+  respuesta: {
+    fontSize: 16,
+  },
+  imageBackground: {
+    alignSelf: "flex-end",
+    width: 210,
+    height: 200,
+  },
 });
 
 export default PreguntasFrecuentes;
-
