4c4210ccd8a675ba3b99decd0c28aa329326d7ac
Yavae <98843718+Yavae@users.noreply.github.com>
2023-12-16 16:05:11 +0100

    Interfaz Screen se foros actualizada

--- cultimate/src/screens/Foro/ForosScreen.jsx
+++ cultimate/src/screens/Foro/ForosScreen.jsx
@@ -1,6 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, ScrollView, TouchableOpacity, Image, StyleSheet, Alert } from 'react-native';
+import { 
+  View, 
+  Text, 
+  ScrollView, 
+  TouchableOpacity, 
+  Image, 
+  StyleSheet, 
+  Alert 
+} from 'react-native';
 import config from '../../../config';
+
+
+const imagenes = {
+  cebolla: require('../../../assets/cebolla.png'),
+  cherry: require('../../../assets/cherry.png'),
+  espinaca: require('../../../assets/espinaca.png'),
+  frambuesa: require('../../../assets/frambuesa.png'),
+  fresa: require('../../../assets/fresa.png'),
+  lechuga: require('../../../assets/lechuga.png'),
+  limon: require('../../../assets/limon.png'),
+  menta: require('../../../assets/menta.png'),
+  mora: require('../../../assets/mora.png'),
+  pepino: require('../../../assets/pepino.png'),
+  pimientos: require('../../../assets/pimientos.png'),
+  tomate: require('../../../assets/tomate.png'),
+  zanahoria: require('../../../assets/zanahoria.png'),
+  calabacin: require('../../../assets/calabacin.png'),
+  brocoli: require('../../../assets/brocoli.png'),
+  albahaca: require('../../../assets/albahaca.png'),
+};
 
 const ForosScreen = ({ navigation }) => {
   const [foros, setForos] = useState([]);
@@ -9,10 +37,10 @@ const ForosScreen = ({ navigation }) => {
     const obtenerForos = async () => {
       try {
         const response = await fetch(`${config.API}/foro`);
-        
+
         if (!response.ok) {
           console.error('Error al obtener los foros', response.status);
-          Alert.alert('Error', 'Hubo un problema al obtener los foros'); // Asegúrate de importar Alert
+          Alert.alert('Error', 'Hubo un problema al obtener los foros');
           return;
         }
 
@@ -20,68 +48,128 @@ const ForosScreen = ({ navigation }) => {
         setForos(data);
       } catch (error) {
         console.error('Error al obtener los foros', error);
-        Alert.alert('Error', 'Hubo un problema al obtener los foros'); // Asegúrate de importar Alert
+        Alert.alert('Error', 'Hubo un problema al obtener los foros');
       }
     };
 
     obtenerForos();
   }, []);
 
-  
-  
-
-
-  const navigateToForo = (foroId) => {
-    navigation.navigate('ForoPlantaScreen', { foroId });
+  const navigateToForo = (foroId,nombreplanta) => {
+    navigation.navigate('ForoPlantaScreen', { foroId, nombreplanta });
   };
 
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      {foros.map((foro) => (
-        <TouchableOpacity
-          key={foro.id}
-          style={styles.foroCard}
-          onPress={() => navigateToForo(foro.id)}
-        >
-          <Image
-            source={foro.imagen ? { uri: foro.imagen } : require('../../../assets/tomate.png')}
-            style={styles.imagenForo}
-          />
-          <Text style={styles.nombreForo}>{foro.nombre}</Text>
-        </TouchableOpacity>
-      ))}
-    </ScrollView>
+    <View style={styles.container}>
+      <View style={styles.backgroundLayer}>
+        <Image source={require('../../../assets/lineales/mora-linea-blanca.png')} style={styles.plantImage} />
+      </View>
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        <Text style={styles.forosTitle}>FOROS</Text>
+        <View style={styles.infoLayer}>
+          {foros.map((foro) => {
+            const nombrePlantaLimpiado = foro.nombre.toLowerCase().replace(/\s+/g, '').replace(/^forode/, '');
+            const imagenPlanta = imagenes[nombrePlantaLimpiado];
+
+            return (
+              <TouchableOpacity
+                key={foro.id}
+                style={styles.foroCard}
+                onPress={() => navigateToForo(foro.id, nombrePlantaLimpiado)}
+              >
+                <View style={styles.foroCardContent}>
+                  <View style={styles.profileImageContainer}>
+                    <Image
+                      source={imagenPlanta}
+                      style={styles.imagenForo}
+                    />
+                  </View>
+                  <Text style={styles.nombreForo}>{foro.nombre}</Text>
+                </View>
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+      </ScrollView>
+    </View>
   );
 };
 
+
 const styles = StyleSheet.create({
   container: {
-    paddingHorizontal: 16,
-    paddingVertical: 8,
+    flex: 1,
+  },
+  backgroundLayer: {
+    backgroundColor: '#09873D',
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+  },
+  forosTitle: {
+    color: '#FFF',
+    fontFamily: 'Integral CF',
+    fontSize: 24,
+    position: 'absolute',
+    top: 165,
+    left: 30,
+    lineHeight: 26,
+  },
+  infoLayer: {
+    backgroundColor: 'white',
+    borderRadius: 20,
+    paddingTop: 20,
+    paddingHorizontal: 5,
+    marginTop: 200,
+    marginBottom: 80,
+    zIndex: 0,
+    paddingBottom: 20, // Añadí un espacio adicional al final
+  },
+  scrollViewContent: {
+    flexGrow: 1, // Ajusté la propiedad para permitir el scroll
   },
   foroCard: {
     flexDirection: 'row',
     alignItems: 'center',
     marginBottom: 16,
-    backgroundColor: '#fff', // Color de fondo de la tarjeta
-    borderRadius: 8,
-    overflow: 'hidden', // Para asegurarse de que las esquinas redondeadas se apliquen correctamente
-    elevation: 4, // Sombra en Android
-    shadowColor: '#000', // Sombra en iOS
-    shadowOffset: { width: 0, height: 2 },
+    backgroundColor: '#F5F5F5', // Fondo ligeramente más oscuro
+    borderRadius: 15,
+    overflow: 'hidden',
+    elevation: 4,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2},
     shadowOpacity: 0.3,
     shadowRadius: 4,
+    padding: 10, // Espaciado interno para separar las tarjetas
   },
-  imagenForo: {
+  foroCardContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flex: 1,
+  },
+  profileImageContainer: {
     width: 80,
     height: 80,
-    resizeMode: 'cover',
-    borderRadius: 8,
+    borderRadius: 15,
+    overflow: 'hidden',
+    marginRight: 12,
+  },
+  imagenForo: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  plantImage: {
+    marginTop: 10,
+    marginLeft: 243,
+    width: 180,
+    height: 180,
   },
   nombreForo: {
     flex: 1,
-    marginLeft: 12,
     fontSize: 16,
+    fontFamily: 'Inter', // Agregué la fuente Inter-Bold'
+    paddingLeft: 20,
   },
 });
 
