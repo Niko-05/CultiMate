c5e3af38a80df12f75743e11b5a7c27e16987b18
Nick <nicolasmz0511@gmail.com>
2024-05-21 18:43:06 +0200

    cambios nuevos

--- node_modules/eas-cli/build/credentials/ios/errors.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AppleTeamMissingError = void 0;
-class AppleTeamMissingError extends Error {
-    constructor(message) {
-        super(message ?? 'Apple Team is necessary to create Apple App Identifier');
-    }
-}
-exports.AppleTeamMissingError = AppleTeamMissingError;
